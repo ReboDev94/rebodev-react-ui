f6e24558cc8a4409e9411e80350f175e75d6b2c1
Rafael de Jesus Rebolledo <rrrrebolledohdz@gmail.com>
2024-07-09 18:00:08 -0600

    fix sidebar v2

--- src/stories/Sidebar/Sidebar.stories.tsx
+++ src/stories/Sidebar/Sidebar.stories.tsx
@@ -1,10 +1,10 @@
-import type { Meta, StoryFn as Story } from '@storybook/react';
-import Sidebar, { ISidebar } from '.';
-// import { IconSearch } from '../assets/svg';
+import type { Meta, StoryObj } from '@storybook/react';
+import { useArgs } from '@storybook/preview-api';
+import Sidebar from '.';
 import { twMerge } from 'tailwind-merge';
 import imageLogo from '../assets/img/logo.png';
 
-export default {
+const meta: Meta<typeof Sidebar> = {
   title: 'GENERAL/Sidebar',
   component: Sidebar,
   tags: ['autodocs'],
@@ -21,71 +21,52 @@ export default {
   },
 } satisfies Meta<typeof Sidebar>;
 
-// const MENU_DATA = [
-//   {
-//     title: 'Dashboard',
-//     url: '/dashboard',
-//     icon: IconSearch,
-//     active: false,
-//   },
-//   {
-//     title: 'Escuela',
-//     url: '/profile/school',
-//     icon: IconSearch,
-//     active: false,
-//   },
-//   {
-//     title: 'Usuarios',
-//     url: '/user',
-//     icon: IconSearch,
-//     active: false,
-//   },
-//   {
-//     title: 'Estudiantes',
-//     url: '/student',
-//     icon: IconSearch,
-//     active: false,
-//   },
-//   {
-//     title: 'Grupos',
-//     url: '/group',
-//     icon: IconSearch,
-//     active: false,
-//   },
-// ];
+export default meta;
+
+type Story = StoryObj<typeof Sidebar>;
 
 const logo = {
   src: imageLogo,
   alt: 'Logo',
 };
 
-export const Default: Story<ISidebar> = (args) => {
-  return (
-    <div className="flex">
-      <Sidebar {...args}>
-        <Sidebar.Header>
-          <img src={logo.src} alt={logo.alt} className="h-20" />
-        </Sidebar.Header>
-        <Sidebar.Content className="px-5 py-6">
-          <Sidebar.Category title="Menu" />
-        </Sidebar.Content>
-        <Sidebar.Footer>
-          <span className="text-sm font-semibold">@Turink {new Date().getFullYear()}</span>
-        </Sidebar.Footer>
-      </Sidebar>
-      <Sidebar.BackDrop onClick={() => {}} className={twMerge(!args.open && 'hidden')} />
-      <main className={twMerge('flex-1 min-w-0 h-screen overflow-y-auto', args.right && 'order-1')}>
-        <div className="px-6 py-8 mx-auto max-w-screen-2xl">
-          <h1>hola mundo</h1>
-        </div>
-      </main>
-    </div>
-  );
-};
-Default.args = {
-  width: 250,
-  minWidth: 250,
-  open: true,
-  right: false,
-  className: '',
+export const Default: Story = {
+  args: {
+    width: 250,
+    minWidth: 250,
+    open: true,
+    right: false,
+    className: '',
+  },
+
+  render: function Render(args) {
+    const [, updateArgs] = useArgs();
+
+    return (
+      <div className="flex">
+        <Sidebar {...args}>
+          <Sidebar.Header>
+            <img src={logo.src} alt={logo.alt} className="h-20" />
+          </Sidebar.Header>
+          <Sidebar.Content className="px-5 py-6">
+            <Sidebar.Category title="Menu" />
+          </Sidebar.Content>
+          <Sidebar.Footer>
+            <span className="text-sm font-semibold">@Turink {new Date().getFullYear()}</span>
+          </Sidebar.Footer>
+        </Sidebar>
+        <Sidebar.BackDrop
+          onClick={() => {
+            updateArgs({ open: false });
+          }}
+          className={twMerge(!args.open && 'hidden')}
+        />
+        <main className={twMerge('flex-1 min-w-0 h-screen overflow-y-auto', args.right && 'order-1')}>
+          <div className="px-6 py-8 mx-auto max-w-screen-2xl">
+            <h1>hola mundo</h1>
+          </div>
+        </main>
+      </div>
+    );
+  },
 };
