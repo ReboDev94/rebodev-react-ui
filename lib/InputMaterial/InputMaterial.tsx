7151e5d33970030511d7ef33dfc054e1c2c7105c
Rafael de Jesus Rebolledo <rrrrebolledohdz@gmail.com>
2024-07-26 14:04:37 -0600

    inputMaterial refactor

--- lib/InputMaterial/InputMaterial.tsx
+++ lib/InputMaterial/InputMaterial.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { IInputMaterial } from './interfaces';
-import { VARIANT_PRIMARY } from '../constants';
+import { SIZE_SM, VARIANT_PRIMARY } from '../constants';
+import Input from '../Input';
 import './InputMaterial.styles.css';
 
-const InputMaterial: React.FC<IInputMaterial> = ({
-  id,
-  children,
-  title,
-  variant = VARIANT_PRIMARY,
-}) => {
-  return (
-    <div className="relative">
-      {children}
-      <label
-        htmlFor={id}
-        className={twMerge('input__material', `input__material__${variant}`)}
-      >
-        {title}
-      </label>
-    </div>
-  );
-};
+const InputMaterial = forwardRef<HTMLInputElement, IInputMaterial>(
+  (props, ref) => {
+    const {
+      id,
+      title,
+      size = SIZE_SM,
+      variant = VARIANT_PRIMARY,
+      ...rest
+    } = props;
 
+    return (
+      <div className="relative">
+        <Input
+          ref={ref}
+          id={id}
+          placeholder=" "
+          type="text"
+          size={size}
+          variant={variant}
+          {...rest}
+        />
+        <label
+          htmlFor={id}
+          className={twMerge('input__material', `input__material__${variant}`)}
+        >
+          {title}
+        </label>
+      </div>
+    );
+  },
+);
+InputMaterial.displayName = 'InputMaterial';
 export default InputMaterial;
