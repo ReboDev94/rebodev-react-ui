b2211eb7bfbb003ff1e72b294616fa42f924fe21
Rafael de Jesus Rebolledo <rrrrebolledohdz@gmail.com>
2024-08-06 18:21:17 -0600

    separate components -bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
 import 'vitest/config';
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import tailwindcss from 'tailwindcss';
 import svgr from 'vite-plugin-svgr';
-import libCss from 'vite-plugin-libcss';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   build: {
+    cssCodeSplit: true,
+    copyPublicDir: false,
     lib: {
       entry: resolve(__dirname, './lib/index.ts'),
+      formats: ['es'],
       name: 'yaret-ui',
-      fileName: format => `index.${format}.js`,
+      // fileName: format => `index.${format}.js`,
     },
     rollupOptions: {
       external: [
@@ -24,7 +34,29 @@ export default defineConfig({
         'tailwindcss',
         'tailwind-merge',
       ],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: [
+              'lib/**/*.d.ts',
+              'lib/**/*.stories.ts',
+              'lib/**/*.stories.tsx',
+              'lib/**/*.test.tsx',
+            ],
+          })
+          .map(file => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
       output: {
+        inlineDynamicImports: false,
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
@@ -40,18 +72,14 @@ export default defineConfig({
   plugins: [
     react(),
     visualizer(),
-    libCss(),
+    libInjectCss(),
     svgr({ include: '**/*.svg?react' }),
     dts({
       rollupTypes: true,
       exclude: ['**/*.stories.ts', '**/*.stories.tsx', '**/*.test.tsx'],
     }),
   ],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
+
   test: {
     globals: true,
     environment: 'jsdom',
